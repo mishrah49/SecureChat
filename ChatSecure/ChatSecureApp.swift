da67dcb209ef230f73fa5dce2a721cb63decf4b5
Harshit <167063767+harshit-pbtech@users.noreply.github.com>
2026-04-01 16:13:33 +0530

    Initial Commit

---
+++ ChatSecure/ChatSecureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSecureApp.swift
+//  ChatSecure
+//
+//  Created by Harshit Mishra on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatSecureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatSecure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatSecure
+//
+//  Created by Harshit Mishra on 01/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
